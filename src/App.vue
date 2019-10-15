fbdf94e08d3b6e7b859ef4080188cd06cfb3d55b
L B Sastry CH <lbsastry@bitbucket.org>
2019-10-15 18:41:27 +0530

    info completed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
     <div class="columns" style="margin-top: 3px;">
 <div class="column is-2" >
      <sidebar> </sidebar>
@@ -33,17 +30,85 @@
                     </b-icon>
                 </a>
                 <p class="header-title">
-                    Component
+                    Order Details
                 </p>
                
             </div>
             <div class="content">
-                <div class="content">
-                   <div>
-                    
-                      <b-field label="Name">
-            <b-input value="Kevin Garvey"></b-input>
-        </b-field>
+                <div class="content ">
+                   <div class="columns is-multiline ">
+                    <div class="column is-6 is-labelstyle " v-for="(products, index) in productList" :key="index">
+                      <label style="padding-right: 75%;">{{products.name}}</label>
+                      <div  style="padding-left:10%">
+            <b-input class="textlabels" :value="products.discripthion" readonly ></b-input>
+
+                      </div>
+                    </div>
+                     
+                   </div>
+                </div>
+            </div>
+        </b-collapse>
+        <b-collapse class="" aria-id="contentIdForA11y3">
+            <div
+                slot="trigger" 
+                slot-scope="props"
+                class="card-header"
+                role="button"
+                aria-controls="contentIdForA11y3">
+                 <a class="header-icon">
+                    <b-icon
+                        :icon="props.open ? 'chevron-down' : 'chevron-up'">
+                    </b-icon>
+                </a>
+                <p class="header-title">
+                    Campaign Details
+                </p>
+               
+            </div>
+            <div class="content">
+                <div class="content ">
+                   <div class="columns is-multiline ">
+                    <div class="column is-6 is-labelstyle " v-for="(products, index) in CampineList" :key="index">
+                      <label style="padding-right: 50%;">{{products.name}}</label>
+                      <div  style="padding-left:10%">
+            <b-input class="textlabels" :value="products.discripthion" readonly ></b-input>
+
+                      </div>
+                    </div>
+                     
+                   </div>
+                </div>
+            </div>
+        </b-collapse>
+        <b-collapse class="" aria-id="contentIdForA11y3">
+            <div
+                slot="trigger" 
+                slot-scope="props"
+                class="card-header"
+                role="button"
+                aria-controls="contentIdForA11y3">
+                 <a class="header-icon">
+                    <b-icon
+                        :icon="props.open ? 'chevron-down' : 'chevron-up'">
+                    </b-icon>
+                </a>
+                <p class="header-title">
+                    Finance Overview
+                </p>
+               
+            </div>
+            <div class="content">
+                <div class="content ">
+                   <div class="columns is-multiline ">
+                    <div class="column is-6 is-labelstyle " v-for="(products, index) in financeList" :key="index">
+                      <label style="padding-right: 38%;">{{products.name}}</label>
+                      <div  style="padding-left:10%">
+            <b-input class="textlabels" :value="products.discripthion" readonly ></b-input>
+
+                      </div>
+                    </div>
+                     
                    </div>
                 </div>
             </div>
@@ -63,6 +128,20 @@
 <script>
 import sidebar from "./Sidebar.vue";
 export default {
+      data(){
+return{
+productList:[{name:"Client",discripthion:"johndeo"},
+{name:"Salesperson",discripthion:"Jasmine Smith"},
+{name:"Satrt Date",discripthion:"jul 9,2018 "},
+{name:"End Date",discripthion:"jul 9,2019"},
+{name:"Orderd Type",discripthion:"regular"},
+{name:"Submited By",discripthion:"Jasmine Smith"}],
+CampineList:[{name:"Business Catogory",discripthion:"Retail Store"}],
+financeList:[{name:"Suggested Display Budget",discripthion:"$1500"},
+{name:"Suggested Search Budget",discripthion:"$1500"},
+{name:"Email",discripthion:"Johndeo@testcompany.com"}]
+}
+    },
   components:{
     "sidebar":sidebar
   }
@@ -75,7 +154,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: #42b983;
 }
 #nav {
   padding: 30px;
@@ -87,7 +165,20 @@ export default {
     }
   }
 }
+.input{
+        color: #d3d7d9 !important;
+    border-color: #fafbfb !important;
+    width: 60% !important;
+}
+.is-labelstyle{
+  
+    color: #d3d7d9;
+    font-size: 18px;
+}
 .headstyle{
       padding-right: 87%;
+}
+.is-multiline{
+  padding-top:3%;
 }
 </style>
